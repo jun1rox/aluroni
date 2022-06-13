24cd23248e682cf13ecb20567880986f97218d8a
jun1rox <junior_alaniz@hotmail.com>
2022-06-13 09:27:15 -0300

    pagina de notfound criada

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Cardapio from 'pages/Cardapio';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PaginaPadrao from 'components/Menu/PaginaPadrao';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
   return (
@@ -15,8 +17,10 @@ export default function AppRouter() {
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
+            <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
