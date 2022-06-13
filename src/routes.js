af8b602c24cb9a6cf81980f6ecbfb29c30900b80
jun1rox <junior_alaniz@hotmail.com>
2022-06-13 09:14:40 -0300

    adicionada pagina sobre e finalizacao do inicio

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Menu from './components/Menu';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PaginaPadrao from 'components/Menu/PaginaPadrao';
 
@@ -10,12 +11,13 @@ export default function AppRouter() {
       <Router>
         <Menu />
         <Routes>
-          <Route path='/' element={<PaginaPadrao/>}>
+          <Route path='/' element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
           </Route>
         </Routes>
-    </Router>
+      </Router>
     </main>
   );
 }
